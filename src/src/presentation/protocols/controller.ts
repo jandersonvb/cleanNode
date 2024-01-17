fb1efda03c360d8f9fff205f8bbd6695f3e4e360
jandersonvb <jandersonvb.dev@gmail.com>
2024-01-17 17:15:51 -0300

    refactor: make SignUpController conforms to a protocol

--- src/src/presentation/protocols/controller.ts
+++ src/src/presentation/protocols/controller.ts
@@ -1,5 +1,7 @@
 import { type HttpRequest, type HttpResponse } from './http'
 
-export interface Controller {
-  handle: (httpRequest: HttpRequest) => Promise<HttpResponse>
+interface Controller {
+  handle: (httpRequest: HttpRequest) => HttpResponse
 }
+
+export type { Controller }
